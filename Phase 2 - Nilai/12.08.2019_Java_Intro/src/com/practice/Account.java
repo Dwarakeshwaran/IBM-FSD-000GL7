86bdcd6bd2a8c188a6ccb849510ae4ef38b6c652
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9.in.ibm.com>
2019-08-12 16:18:09 +0530

    Reference Comparison

---
+++ Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/Account.java
@@ -0,0 +1,58 @@
+package com.practice;
+
+public class Account {
+	private int salary;
+	private String name;
+	
+	public Account(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + salary;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (salary != other.salary)
+			return false;
+		return true;
+	}
+	
+	
+
+}
