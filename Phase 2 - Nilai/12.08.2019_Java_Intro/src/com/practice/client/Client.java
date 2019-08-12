03d4b893326f4e631a0ccb38c615a1763c760602
Dwarakeshwaran B M <dwmages1@in.ibm.com>
2019-08-12 14:05:18 +0530

    Constructor Chaining Finished

--- Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/client/Client.java
+++ Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/client/Client.java
@@ -4,14 +4,14 @@ import com.practice.client.ConstructorChaining;
 public class Client {
 
 	public static void main(String[] args) {
-//		MyDate myDate = new MyDate();
-//		myDate.setDay(17);
-//		myDate.setMonth(22);
-//		myDate.setYear(1998);
-//		
-//		myDate.getDay();
-//		myDate.getMonth();
-//		myDate.getYear();
+		MyDate myDate = new MyDate();
+		myDate.setDay(17);
+		myDate.setMonth(02);
+		myDate.setYear(1998);
+		System.out.println(myDate.getDay());
+		System.out.println(myDate.getMonth());
+		myDate.getYear();
+		
 		
 		ConstructorChaining cc = new ConstructorChaining(20);
 		System.out.println(cc.getDetails());
