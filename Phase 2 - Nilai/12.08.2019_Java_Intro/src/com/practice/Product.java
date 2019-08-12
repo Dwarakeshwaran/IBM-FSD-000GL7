7c61b7390a7864509b8c11e8f59b22cca17d6793
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9.in.ibm.com>
2019-08-12 17:16:57 +0530

    Product Assignment

---
+++ Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/Product.java
@@ -0,0 +1,58 @@
+//Blank final variable
+//final methods cannot be overridden
+//final classes cannot be extended for eg, String Class cannot be extended since it is a Final Class
+
+package com.practice;
+
+public class Product {
+	private int currentRate;
+	
+	private static int item;
+	
+	public static int getItem() {
+		return item;
+	}
+
+	public int getCurrentRate() {
+		return currentRate;
+	}
+
+	public void setCurrentRate(int currentRate) {
+		this.currentRate = currentRate;
+	}
+
+	static {
+		item=0;
+	}
+	public static void main(String[] args) {
+		Product pro = new Product();
+		pro = new Book();
+		
+		System.out.println(Product.getItem());
+		pro = new Tv();
+		System.out.println(Product.getItem());
+		//System.out.println(book.item);
+		
+		
+	}
+
+}
+
+class Book extends Product{
+	public String name;
+	public String author;
+	
+	
+}
+
+class Electronics extends Product{
+	public int mYear;
+}
+
+class Tv extends Electronics{
+	public int rate;
+	
+
+}
+
+
