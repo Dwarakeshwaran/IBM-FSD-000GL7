30977ed951e65ed4cefaef9f8b469dae5cc77f12
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9.in.ibm.com>
2019-08-13 15:56:12 +0530

    Array Push and Pop Assignment Finished

--- Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/array/ArrayJava.java
+++ Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/array/ArrayJava.java
@@ -2,26 +2,67 @@ package com.practice.array;
 
 public class ArrayJava {
 	
-	private char[] arr;
-	
+	private char[] charArr;
+	private int[] intArr;
+	public int length;
+	public int top = 0;
 	//Below is nothing but the getter and setter for the Array
 	public void createArray(int length)
 	{
-		arr = new char[length];
+		charArr = new char[length];
 		
 	}
 	
+	
 	public void setArray()
 	{
-		for(int i = 0; i<arr.length; i++)
+		for(int i = 0; i<charArr.length; i++)
 		{
-			arr[i] = (char) ('A'+i);//A should be single quotes
+			charArr[i] = (char) ('A'+i);//A should be single quotes
 		}
 	}
 	
 	public char[] getArray()
 	{
-		return arr;
+		return charArr;
+	}
+	
+	public void createIntArray(int length)
+	{
+		intArr = new int[length];
+		this.length = length;
+	}
+	
+	public void push(int value)
+	{
+		if(this.length <= top)
+			System.out.println("Overflow!");
+		else
+		{
+			intArr[top] = value; 
+			top++;
+		}
+		
+	}
+	public void pop()
+	{
+		if(top <= 0)
+			System.out.println("Underflow!");
+		else
+		{
+			intArr[top-1]=0;
+			top--;
+		}
+			
+		
+	}
+	
+	public void getIntArray()
+	{
+		for(int i = 0; i<intArr.length ; i++)
+		{
+			System.out.print(intArr[i] + " ");
+		}
 	}
 
 }
--- Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/array/Main.java
+++ Phase 2 - Nilai/12.08.2019_Java_Intro/src/com/practice/array/Main.java
@@ -2,6 +2,7 @@ package com.practice.array;
 
 import java.util.Arrays;
 
+
 public class Main {
 	public static void main(String[] args) {
 		ArrayJava arr = new ArrayJava();
@@ -9,15 +10,42 @@ public class Main {
 		arr.setArray();
 		System.out.println(arr.getArray());
 		
-		int[] array = {11,33,10,9,5};
 		
-		Arrays.sort(array); 
+		
+		int[] array = {11,33,10,9,5};
+		int[] arrayReverse= {};
+		Arrays.sort(array); //bubble sort
+//		for(int i = 0 , j = array.length-1; i < array.length && j >= 0  ; i++ , j--)
+//		{
+//				arrayReverse[i] = array[j];
+//		}
 		
 		for(int i=0;i<array.length;i++)
 			System.out.print(" " + array[i]);
-		
+//		for(int i=0;i<array.length;i++)
+//			System.out.print(" " + arrayReverse[i]);
 		
 		System.out.println("\n INDEX: " + Arrays.binarySearch(array, 33));
+	
+		
+		
+		ArrayJava intArray = new ArrayJava();
+		
+		intArray.createIntArray(5);
+		intArray.push(3);
+		intArray.push(6);
+		intArray.push(9);
+		intArray.push(12);
+		intArray.push(15);
+		intArray.pop();
+		intArray.pop();
+		intArray.pop();
+		intArray.pop();
+		intArray.pop();
+		intArray.pop();
+		
+		intArray.getIntArray();
 	}
+	
 
 }
