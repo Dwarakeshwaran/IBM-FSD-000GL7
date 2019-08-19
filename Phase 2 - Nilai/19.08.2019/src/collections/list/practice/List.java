6cd3e046e09ce727d244808bfbeb66845fa6a43c
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9.in.ibm.com>
2019-08-19 10:00:50 +0530

    List Completed

---
+++ Phase 2 - Nilai/19.08.2019/src/collections/list/practice/List.java
@@ -0,0 +1,16 @@
+package collections.list.practice;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class List {
+	public static void main(String[] args) {
+		Collection<String> list = new ArrayList<String>();
+		list.add("Dwaki");
+		list.add("Suman");
+		
+		System.out.println(list);
+		
+	}
+
+}
