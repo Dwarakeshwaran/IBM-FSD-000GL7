b2c6998bb967513dd0ab84449301dc825bd7d486
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9.in.ibm.com>
2019-08-19 10:57:25 +0530

    Map and Vector Finished

---
+++ Phase 2 - Nilai/19.08.2019/src/collections/map/practice/MapDemo.java
@@ -0,0 +1,21 @@
+package collections.map.practice;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("Name", "Dwaki");
+		map.put("CodeName", "Kettavan");
+		map.put("CodeNumber", "007");
+		
+		System.out.println(map.entrySet());
+		map.put("Name", "Suman");
+		System.out.println(map.entrySet());
+
+	}
+
+}
---
+++ Phase 2 - Nilai/19.08.2019/src/collections/vector/practice/VectorDemo.java
@@ -0,0 +1,20 @@
+package collections.vector.practice;
+import java.util.Collections;
+import java.util.Vector;
+
+
+public class VectorDemo {
+	
+
+	public static void main(String[] args) {
+		
+		Vector<String> vector = new Vector<String>();
+		vector.add("a");
+		vector.add("b");
+		vector.add("c");
+		System.out.println();
+		
+
+	}
+
+}
