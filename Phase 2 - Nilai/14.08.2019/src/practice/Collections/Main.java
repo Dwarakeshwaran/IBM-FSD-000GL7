2a9dcd13196da90de5d1ab7d7913419f298497dc
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9.in.ibm.com>
2019-08-19 08:59:08 +0530

    Collections Assignment Completed

--- Phase 2 - Nilai/14.08.2019/src/practice/Collections/Main.java
+++ Phase 2 - Nilai/14.08.2019/src/practice/Collections/Main.java
@@ -1,10 +1,43 @@
 package practice.Collections;
 
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
-
-	public static void main(String[] args) {
-		
-
-	}
+    
+    static Scanner sc = new Scanner(System.in);
+    private String str[];
+    private Set<String> set;
+    
+    public Main() throws InputMismatchException {
+        
+        System.out.print("Number of Elements in the Array: ");
+        str = new String[sc.nextInt()];
+        for(int i=0; i<str.length; i++) {
+            System.out.println("Name: ");
+            str[i]=sc.next().toString().toLowerCase();
+        }
+        set = new HashSet<String>();
+        for(String s: str) {
+            if(!set.add(s))
+            {
+                System.out.println("Duplicate element cannot be added...");
+            }
+        }
+    }
+    
+    public void displayCollection() {
+        for(String ss: set) {
+            System.out.println(ss.toString());
+        }
+        //iterator should be used for good practice
+        Iterator<String> i =set.iterator();
+        while(i.hasNext()) {
+            System.out.println(i.next().toString());
+        }
+    }
 
 }
