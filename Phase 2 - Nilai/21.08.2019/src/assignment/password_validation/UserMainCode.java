258b21590b09f223cc7a0402caa56841c6f6f462
DwarakeshwaranBM <DwarakeshwaranBM@DESKTOP-74LMUR9>
2019-08-21 22:18:31 +0530

    Assignment 2 Password Validation Completed

---
+++ Phase 2 - Nilai/21.08.2019/src/assignment/password_validation/UserMainCode.java
@@ -0,0 +1,39 @@
+package assignment.password_validation;
+
+public class UserMainCode {
+	public static boolean checkPassword(String password) {
+
+		final String specialChars = "~`!@#$%^&*()-_=+\\|[{]};:'\",<.>/?";
+
+		boolean upperCase = false;
+		boolean lowerCase = false;
+		boolean number = false;
+		boolean splchar = false;
+		int flag = 0;
+
+		if (password.length() >= 8) {
+			for (int i = 0; i < password.length(); i++) {
+				char ch = password.charAt(i);
+				if (Character.isUpperCase(ch))
+					upperCase = true;
+				else if (Character.isLowerCase(ch))
+					lowerCase = true;
+				else if (Character.isDigit(ch))
+					number = true;
+				else if (specialChars.contains(String.valueOf(ch)))
+					splchar = true;
+			}
+			if (upperCase == true && lowerCase == true && number == true && splchar == true)
+				flag=1;;
+		}
+		else
+			flag=0;		
+		
+		if(flag==1)
+			return true;
+		else
+			return false;
+	}
+	
+	
+}
